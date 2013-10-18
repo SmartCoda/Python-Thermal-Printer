91691f1a82715777486d83d165a73f25f35aed63
SmartCoda <i.smartcoda@gmail.com>
2013-10-18 08:18:37 +0100

    updated test code

--- imgbw.py
+++ imgbw.py
@@ -1,5 +1,6 @@
 from PIL import Image 
 image_file = Image.open("gfx/partlycloudy.gif") # open colour image
+#image_file = image_file.resize((100,100),Image.ANTIALIAS)
 image_file = image_file.convert('L') # convert image to black and white
 image_file.save('result.png')
 image_file = Image.open("result.png") # open colour image
--- pimage.py
+++ pimage.py
@@ -1,3 +1,4 @@
+
 from __future__ import print_function
 import RPi.GPIO as GPIO
 import subprocess, time, Image, socket
@@ -13,14 +14,20 @@ ht = printer.defaultHeatTime * 2
 if(ht > 255): ht = 255
 
 printer.begin(ht) # Set temporary dark heat time
-printer.println("Colour")
-printer.printImage(Image.open('gfx/partlycloudy.gif'), True)
+image_file = Image.open('gfx/partlycloudy.gif')
+image_100 = image_file.resize((100,100),Image.ANTIALIAS)
+image_200 = image_file.resize((200,200),Image.ANTIALIAS)
+image_file = image_file.convert('L')
+image_100 = image_100.convert('L')
+image_200 = image_200.convert('L')
+printer.println("Image 50x50")
+printer.printImage(image_file, True)
 printer.feed(3)
-printer.println("Greyscale")
-printer.printImage(Image.open('gfx/result.png'), True)
+printer.println("Image 100x100")
+printer.printImage(image_100, True)
 printer.feed(3)
-printer.println("B&W")
-printer.printImage(Image.open('gfx/result.gif'), True)
+printer.println("Image 200x200")
+printer.printImage(image_200, True)
 printer.feed(3)
 printer.begin() # Reset default heat time
 
