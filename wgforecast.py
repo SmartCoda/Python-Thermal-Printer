49facdc4d7d0621fff273f2239338fdc43d04a16
SmartCoda <i.smartcoda@gmail.com>
2013-10-27 12:49:13 +0000

    start of wunderground forecast

---
+++ wgforecast.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#
+# Forecast for adafruit raspberry pi thermal
+# internet of things printer.
+# This version uses wunderground for the
+# weather data. Later revisions will have
+# weather icon support.
+# 
+
